3a9aa4eb0eb99f83fbdcf888b4d29e94e6a87557
matonya19 <matonya19@mail.ru>
2023-01-29 20:59:13 +0400

    function GetShortWordArray

--- program/Program.cs
+++ program/Program.cs
@@ -5,4 +5,22 @@
 	string[] arr2 = {"1234", "1567", "-2", "computer science"};
 	string[] arr3 = {"Russia", "Denmark", "Kazan"};
 		
-	
+	string[] GetShortWordsArray(string[] array)
+        {
+            int wordLength = array.Length;          
+            string[] result = new string[wordLength];
+            int count = 0;
+            
+            for (int i = 0; i < wordLength; i++)
+            {
+                if(array[i].Length <= 3)
+                {
+                    result[count] = array[i];
+                    count++;
+                }
+            }
+            Array.Resize(ref result, count);
+            
+            return result;
+        }
+        
